55e637aa806cd5d9aa4df5b4e5337b1bc9e06389
Sergey Nikolaev <107795096+Brutozubo@users.noreply.github.com>
2022-09-04 22:23:27 +0500

    Update part5_2.py

--- part5_2.py
+++ part5_2.py
@@ -5,12 +5,14 @@ class Model:
     author = '3'
 
     def save(self):
+        import json
+        
         model_a = Model()
         model_a.save()
         list = filter(lambda x: not x.startswith('_'), dir(model_a)))
         print(list)
         
-        import json
+        
         json = json.dumps(dict)
         f = open('dict.json', 'w')
         f.write(json)
