f177589a167c44f45dfa5b6d2c09ca6d869203da
Sergey Nikolaev <107795096+Brutozubo@users.noreply.github.com>
2022-09-13 16:18:27 +0500

    Update part5_2.py
    Some commit

--- part5_2.py
+++ part5_2.py
@@ -1,19 +1,21 @@
-class Model:
+import json
 
-    title = '1'
-    text = '2'
-    author = '3'
+class Model():
+    def __init__(self, title, text, author):
+        self.title = 1
+        self.text = 2
+        self.author = 3
+data = {"Title": 1, "Text": 2, "Author": 3}
+obj = Model(1, 2, 3)
 
-    def save(self):
-        import json
-        
-        model_a = Model()
-        model_a.save()
-        list = filter(lambda x: not x.startswith('_'), dir(model_a)))
-        print(list)
-        
-        
-        json = json.dumps(dict)
-        f = open('dict.json', 'w')
-        f.write(json)
-        f.close()
+
+fltrd_lst = [a for a in dir(obj) if not a.startswith('__')]
+print(fltrd_lst)
+
+print(obj.title)
+print(obj.text)
+print(obj.author)
+
+json_string = json.dumps(data)
+with open('json_data.json', 'w') as outfile:
+    outfile.write(json_string)
