08229cd86ff4dfd453ac4c12e7d416a0e33fea48
Sergey Nikolaev <107795096+Brutozubo@users.noreply.github.com>
2022-09-14 13:01:59 +0500

    Update part5_2.py

--- part5_2.py
+++ part5_2.py
@@ -1,21 +1,20 @@
+
 import json
 
 class Model():
     def __init__(self, title, text, author):
-        self.title = 1
-        self.text = 2
-        self.author = 3
-data = {"Title": 1, "Text": 2, "Author": 3}
+        self.title = title
+        self.text = text
+        self.author = author
+
 obj = Model(1, 2, 3)
 
 
 fltrd_lst = [a for a in dir(obj) if not a.startswith('__')]
-print(fltrd_lst)
 
-print(obj.title)
-print(obj.text)
-print(obj.author)
 
-json_string = json.dumps(data)
+json_string = json.dumps(obj.__dict__)
 with open('json_data.json', 'w') as outfile:
     outfile.write(json_string)
+
+print(json_string)
